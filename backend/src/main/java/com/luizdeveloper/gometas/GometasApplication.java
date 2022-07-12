083409e9b769d1422f57a54698fce3a4728df497
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-07-12 16:49:26 -0300

    Project created

---
+++ backend/src/main/java/com/luizdeveloper/gometas/GometasApplication.java
@@ -0,0 +1,13 @@
+package com.luizdeveloper.gometas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GometasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GometasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luizdeveloper/gometas/GometasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdeveloper.gometas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GometasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
