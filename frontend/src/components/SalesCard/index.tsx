fab09c17d6480e21a2772c763a853df72979df3a
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-07-13 08:15:06 -0300

    Add Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,89 +1,89 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
+import "./styles.css";
 
-import NotificationButton from '../NotificationButton'
-import './styles.css'
-
-
-function SalesCard(){
-
-    return(
-
-        <div className="goMetas-card">
-        <h2 className="goMetas-sales-title">Vendas</h2>
-        <div>
-          <div className="goMetas-form-control-container">
-            <input className="goMetas-form-control" type="text"  />
-          </div>
-          <div className="goMetas-form-control-container">
-            <input className="goMetas-form-control" type="text" />
-          </div>
+function SalesCard() {
+  return (
+    <div className="goMetas-card">
+      <h2 className="goMetas-sales-title">Vendas</h2>
+      <div>
+        <div className="goMetas-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="goMetas-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
-
-        <div>
-          <table className="goMetas-sales-table">
-            <thead>
-              <tr>
-                <th className="show992">ID</th>
-                <th className="show576">Data</th>
-                <th>Vendedor</th>
-                <th className="show992">Visitas</th>
-                <th className="show992">Vendas</th>
-                <th>Total</th>
-                <th>Notificar</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="goMetas-red-btn-container">
-                    
-                    <NotificationButton/>
-
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="goMetas-red-btn-container">
-                    
-                    <NotificationButton/>
-
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
-                <td>R$ 55300.00</td>
-                <td>
-                  <div className="goMetas-red-btn-container">
-                    
-                  <NotificationButton/>
-                  
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-
-          </table>
+        <div className="goMetas-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="goMetas-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
-
       </div>
-    )
+
+      <div>
+        <table className="goMetas-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="goMetas-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="goMetas-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="show992">#341</td>
+              <td className="show576">08/07/2022</td>
+              <td>Anakin</td>
+              <td className="show992">15</td>
+              <td className="show992">11</td>
+              <td>R$ 55300.00</td>
+              <td>
+                <div className="goMetas-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default SalesCard
+export default SalesCard;
