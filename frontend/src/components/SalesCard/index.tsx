7fcf1aa48c023fc5c7fc9c7a185a16996d7eb55d
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-07-18 11:58:35 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,9 +19,12 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
   useEffect(() => {
 
-   axios.get(`${BASE_URL}/sales`).then(Response => {setSales(Response.data.content);});
+    const dmin = minDate.toISOString().slice(0, 10); 
+    const dmax = maxDate.toISOString().slice(0, 10);
 
-  }, []);
+   axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(Response => {setSales(Response.data.content);});
+
+  }, [minDate, maxDate]);
 
 
   return (
